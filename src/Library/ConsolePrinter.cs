43e452f9cf9933d5e4164d33622b0558f8692bae
sebastian-freire <SEBASTIAN.FREIRE@correo.ucu.edu.uy>
2024-04-14 00:02:46 -0300

    First commit

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Full_GRASP_And_SOLID
+{
+    public class ConsolePrinter
+    {
+        public static void Imprimir(StringBuilder impresion)
+        {
+            
+            Console.WriteLine(impresion);
+        }
+    }
+}
